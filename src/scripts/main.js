604018880e739edb7e0a0e8ccf7dce279c8606dd
Valentyna Bahyrly <valentynabahyrly@gmail.com>
2026-03-27 20:42:01 +0200

    Fixed: cloneNode

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,16 +5,12 @@ const table = document.querySelector('table');
 for (let i = 0; i < table.rows.length; i++) {
   const row = table.rows[i];
 
-  // Додаємо нову комірку
-  const targetIndex = row.cells.length - 1;
-  const newCell = row.insertCell(targetIndex >= 0 ? targetIndex : 0);
+  const cellToClone = row.cells[1];
 
-  // Додаємо контент у нову комірку
-  const secondCellValue = row.cells[1].textContent;
+  if (cellToClone) {
+    const clonedCell = cellToClone.cloneNode(true);
+    const lastCell = row.cells[row.cells.length - 1];
 
-  newCell.textContent = secondCellValue;
-
-  if (i === table.rows.length - 1) {
-    newCell.style.fontWeight = 'bold'; // Робимо жирним тільки тут
+    row.insertBefore(clonedCell, lastCell);
   }
 }
