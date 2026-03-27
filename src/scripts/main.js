6647e5ef6cb922eed5f7cf77d88e24ce7661a4b5
Valentyna Bahyrly <valentynabahyrly@gmail.com>
2026-03-27 20:08:45 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+
+  // Додаємо нову комірку
+  const targetIndex = row.cells.length - 1;
+  const newCell = row.insertCell(targetIndex >= 0 ? targetIndex : 0);
+
+  // Додаємо контент у нову комірку
+  const secondCellValue = row.cells[1].textContent;
+
+  newCell.textContent = secondCellValue;
+
+  if (i === table.rows.length - 1) {
+    newCell.style.fontWeight = 'bold'; // Робимо жирним тільки тут
+  }
+}
